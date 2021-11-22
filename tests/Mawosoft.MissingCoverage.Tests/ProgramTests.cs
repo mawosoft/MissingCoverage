b7dc77c80cf941816ee138fff26e4fa48c35b98a
mawosoft <78562192+mawosoft@users.noreply.github.com>
2021-11-22 22:16:35 +0100

    Minimal test added.

---
+++ tests/Mawosoft.MissingCoverage.Tests/ProgramTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2021 Matthias Wolf, Mawosoft.
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Mawosoft.MissingCoverage.Tests
+{
+    public class ProgramTests
+    {
+        private readonly ITestOutputHelper _testOutput;
+        public ProgramTests(ITestOutputHelper testOutput) => _testOutput = testOutput;
+
+        private class RedirectWriter : TextWriter
+        {
+            private readonly ITestOutputHelper _testOutput;
+            public RedirectWriter(ITestOutputHelper testOutput) => _testOutput = testOutput;
+            public override Encoding Encoding => Encoding.Unicode;
+            public override void Write(char value) => throw new NotImplementedException();
+            public override void WriteLine(string? value) => _testOutput.WriteLine(value);
+        }
+
+        private static string TestDataDirectory()
+        {
+            string path = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                ?? Directory.GetCurrentDirectory(),
+                "../../../../testdata"));
+            if (!Path.EndsInDirectorySeparator(path)) path += '/';
+            return path;
+        }
+
+        // TODO Add proper tests.
+        // This is just for coverage and manual validation.
+        [Theory]
+        [InlineData("-h")]
+        [InlineData(null)]
+        [InlineData("-ht", "0", "{testdata}*")]
+        [InlineData("-ht", "0", "-bt", "4", "{testdata}*")]
+        [InlineData("{testdata}coverlet big.xml")]
+        [InlineData("{testdata}coverlet small.xml")]
+        [InlineData("{testdata}fcc merged.xml")]
+        [InlineData("{testdata}coverlet*.xml")]
+        [InlineData("-lo", "{testdata}*")]
+        public void Run_CmdlineArguments(params string[]? args)
+        {
+            if (args != null)
+            {
+                string path = TestDataDirectory();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    args[i] = args[i].Replace("{testdata}", path, StringComparison.Ordinal);
+                }
+            }
+            RedirectWriter redirect = new(_testOutput);
+            Program.Out = redirect;
+            Program.Error = redirect;
+            //Program program = new();
+            //program.Run(args ?? Array.Empty<string>());
+            Program.Main(args ?? Array.Empty<string>());
+        }
+
+    }
+}
